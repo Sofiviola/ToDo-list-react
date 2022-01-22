cc3078cd307c52564e31d10f461e9a1a96ad5775
SofiViola <Sofiaviola524@gmail.com>
2022-01-22 09:41:36 -0300

    estructura base

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from './componentes/Header'
+
+
+const App = () => {
+  return (
+  <div className="contenedor">
+    <Header/>
+  </div>
+  );
+}
+
+export default App;
